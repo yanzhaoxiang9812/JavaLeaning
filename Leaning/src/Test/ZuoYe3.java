86d0d8c7e248b61daeee2a4614ce14c69e42b4c1
闫兆祥 <73688938+yanzhaoxiang9812@users.noreply.github.com>
2021-03-26 19:40:10 +0800

    Add files via upload

---
+++ Leaning/src/Test/ZuoYe3.java
@@ -0,0 +1,71 @@
+package ZuoYe;
+/*
+4、编写Java程序模拟简单的计算器。
+定义名为Number的类其中有两个整型数据成员n1和n2应声明为私有。
+编写构造方法赋予n1和n2初始值再为该类定义加addition()、减subtration()、
+乘multiplication()、除division()等
+公有实例方分别对两个成员变量执行加、减、乘、除的运算。
+
+在main方法中创建Number类的对象调用各个方法并显示计算结果
+
+*/
+public class ZuoYe3 {
+    public static void main(String[] args) {
+        Number n1 = new Number();
+        System.out.println("计算结果为：" + n1.addition());
+        System.out.println("计算结果为：" + n1.subtration());
+        System.out.println("计算结果为：" + n1.multiplication());
+        System.out.println("计算结果为：" + n1.division());
+        System.out.println("================================");
+        Number n2 = new Number(6,6);
+        System.out.println("计算结果为：" + n2.addition());
+        System.out.println("计算结果为：" + n2.subtration());
+        System.out.println("计算结果为：" + n2.multiplication());
+        System.out.println("计算结果为：" + n2.division());
+    }
+}
+class Number{
+    private int n1;
+    private int n2;
+    public Number() {
+    }
+    public Number(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+    public int getN1() {
+        return n1;
+    }
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+    public int getN2() {
+        return n2;
+    }
+    public void setN2(int n2) {
+        this.n2 = n2;
+    }
+
+    public int addition(){
+        int jia = this.getN1()+this.getN2();
+        return jia;
+    }
+    public int subtration(){
+        int jian = this.getN1() + this.getN2();
+        return jian;
+    }
+    public int multiplication(){
+        int cheng = this.getN1() * this.getN2();
+        return cheng;
+    }
+    public double division(){
+        if(getN2() == 0){
+            System.out.println("除数不能更改为零！");
+            return 0;
+        }else {
+            double chu = this.getN1() / this.getN2();
+            return chu;
+        }
+
+    }
+}
---
+++ Leaning/src/Test/ZuoYe4.java
@@ -0,0 +1,39 @@
+package ZuoYe;
+/*编写Java程序用于显示人的姓名和年龄。
+定义一个人类Person该类中应该有两个私有属性姓名name和年龄age。
+定义构造方法用来初始化数据成员。再定义显示display方法将姓名和年龄打印出来。
+在main方法中创建人类的实例然后将信息显示
+*/
+
+public class ZuoYe4 {
+    public static void main(String[] args) {
+        Person person = new Person("张三",22);
+        person.display();
+    }
+}
+class Person{
+    private String name;
+    private int age;
+    public void display(){
+        System.out.println("姓名：" + getName() + "    年龄：" + getAge());
+    }
+    public Person() {
+    }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
