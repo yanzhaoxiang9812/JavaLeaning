241deda917acfd6966099d1307fcf312e56eab31
闫兆祥 <73688938+yanzhaoxiang9812@users.noreply.github.com>
2021-04-12 22:55:05 +0800

    Add files via upload

---
+++ Leaning/src/IO_/IO_Test1.java
@@ -0,0 +1,32 @@
+package IO_;
+/*
+            IO：输入输出
+                Input输入   Output输出
+
+ 流的分类：
+    按照方向分类：
+        往内存中去，叫做输入/读（Input/Reader）
+        从内存中出，叫做输出/写（Output/Writer）
+    按照读取数据方式分类：
+        字节流：
+            按字节读取，一次读取一个字节包括任何类型的文件。
+        字符流：
+            按字符读取，一次读取一个字符，仅支持普通文本文件。
+
+    JAVA四大抽象类：
+        java.io.InputStream         字节输入流
+        java.io.OutputStream        字节输出流
+        java.io.Reader              字符输入流
+        java.io.Writer              字符输出流
+            这四个抽象类都实现了java.io.Closeable接口，都是可关闭的。
+            所有‘输出流‘都实现了java.io.Flushable接口，都有flush()方法。作用为刷新。
+        *类名以Stream结尾为字节流，以Reader/Writer结尾为字符流*
+
+                ***流在使用完毕后切记关闭***
+
+ */
+public class IO_Test1 {
+    public static void main(String[] args) {
+
+    }
+}
